fe2900be8cca16040daec84d973865b83bae97c5
GbadamosiMuibat <81063006+GbadamosiMuibat@users.noreply.github.com>
2024-09-24 01:55:22 -0700

    Initial commit

---
+++ Madlib.py
@@ -0,0 +1,20 @@
+#Madlib is a word game where you create a story by filling in a random words
+
+place = input("Enter any name of place: ")
+name = input("Enter any name of person:  ")
+action = input("Enter any action ending with 'ing':  ")
+object =input("Enter any name of tangible object:  ")
+abstract = input("Enter your emotion or value: ")
+animal = input("Enter any name of animal: ")
+place_of_worship = input("Enter any name of place of worship: ")
+thing =input("Enter any name of thing(something you can be thanful for): ")
+
+print(f"""Once upon a time, in the peaceful town of {place}, there lived a young person named {name}. 
+Every morning, {name} would start their day by performing {action} to seek peace and guidance. 
+On this special day, as the sun rose, the light reflected beautifully off the {object}  in the room,
+reminding {name} of the beauty in {abstract}.
+After {name} finished their morning routine, they decided to visit the local {place_of_worship}. 
+As they walked, they were greeted by the sound of {animal} in the distance. 
+The fresh air and calm surroundings brought a sense of gratitude into their heart, making them think about the blessings of {thing}.
+At the end of the day, {name} sat down to reflect on everything they experienced, 
+knowing that tomorrow would bring new opportunities for {abstract}.""")
